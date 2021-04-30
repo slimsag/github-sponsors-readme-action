c96e9becb04e1c3c3b0ffa9a7b54cd4959625a8d
James Ives <iam@jamesiv.es>
2021-04-30 12:51:41 -0400

    t

--- lib/fetch.d.ts
+++ lib/fetch.d.ts
@@ -1,4 +1,4 @@
-import 'cross-fetch/polyfill'
-export declare function retrieveData(): Promise<Record<string, unknown>>
-export declare function generateTemplate(response: any): string
-export declare function generateFile(response: any): Promise<void>
+import 'cross-fetch/polyfill';
+export declare function retrieveData(): Promise<Record<string, unknown>>;
+export declare function generateTemplate(response: any): string;
+export declare function generateFile(response: any): Promise<void>;
