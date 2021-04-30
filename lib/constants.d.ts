dba26090774df8ba586b846b52f5f38770d69615
James Ives <iam@jamesiv.es>
2021-04-30 12:24:53 -0400

    t

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,7 +1,30 @@
 export interface ActionInterface {
-  /** The template to use. */
-  template: string
+    /** The template to use. */
+    template: string;
 }
 export declare const action: {
-  template: string
+    template: string;
+};
+export declare enum PrivacyLevel {
+    PUBLIC = "PUBLIC",
+    PRIVATE = "PRIVATE"
+}
+export interface Sponsor {
+    sponsorEntity: {
+        name: string | null;
+        login: string;
+        url: string;
+    };
+    createdAt: string;
+    privacyLevel: PrivacyLevel;
+    tier: {
+        monthlyPriceInCents: number;
+    };
+}
+export interface SponsorshipsAsMaintainer {
+    totalCount: number;
+    pageInfo: {
+        endCursor: string;
+    };
+    nodes: Sponsor[];
 }
